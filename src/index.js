15aff8698b3757c62d250329004800097698311c
AlexhahahaDrag <734663446@qq.com>
2018-12-12 16:26:31 +0800

    practice webpack twice

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
  import _ from 'lodash';
+ import printMe from './print.js';
  import './style.css';
- import Icon from './v2-be203bf3c47eca294c5c3bc4e1e03b40_r.jpg'
-
+ import Icon from './icon.png';
   function component() {
     var element = document.createElement('div');
-    // lodash 是由当前 script 脚本 import 导入进来的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    //将图片添加打我们有的div
-    var myIcon=new Image();
-    myIcon.src=Icon;
-    element.appendChild(myIcon);
+   var btn = document.createElement('button');
+   var myIcon = new Image();
+   myIcon.src=Icon;
+   
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   btn.innerHTML = 'Click me and check the console!';
+   btn.onclick = printMe;
+   element.appendChild(btn);
+element.appendChild(myIcon);
     return element;
   }
 
